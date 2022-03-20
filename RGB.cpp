7a62d6b751a413e1d209c758ff5e9acdf575371b
Jacob Hagberg <58997374+JacobHagberg@users.noreply.github.com>
2022-03-19 21:29:50 -0500

    Made an array storage array movable

--- RGB.cpp
+++ RGB.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <unistd.h>
+#include<link.h>
 using namespace std;
 
 #include "RGB.h"
-
 
 RGB::RGB()
 {
@@ -43,12 +44,13 @@ RGB::~RGB()
 	// Call lower library function to cleanup DMA
 	ws2811_fini(&ledString);
 }
-
+RGB rgb;
 
 void RGB::SetAll(ws2811_led_t color) {
 	for (uint8_t x = 0; x < ledCount; x++) {
 		ledString.channel[0].leds[x] = color;
 	}
+	cout<<"Set all"<<endl;
 }
 
 void RGB::Set(uint8_t index, ws2811_led_t color) {
@@ -66,13 +68,79 @@ void RGB::Render() {
 		exit(ret);
         //cout<<"render error"<<endl;
 	}
+}  
+
+void RGB::UniversalClock(int msWait){
+	usleep(msWait*1000);
 }
 
-RGB rgb;
+void RGB::RenderArray(){
+	for(int i = 0; i<COLORS::numberOfLeds; i++){
+		rgb.Set(i,rgb.LEDMap[i].color);
+	}
+	Render();
+}
+
+void RGB::MoveArray(bool wrapAround, bool forward, int moveNumber){
+	cout<<"MoveArray: a";
+	for(int a = 0; a<moveNumber;a++){
+		cout<<"b";
+		if(forward){
+			cout<<"c";
+			rgb.LEDStore = rgb.LEDMap[COLORS::numberOfLeds - 1];
+			for(int b = COLORS::numberOfLeds - 1;b>0;b--){
+				rgb.LEDMap[b] = rgb.LEDMap[b-1];
+				cout<<b;
+		}
+		if(wrapAround){
+			rgb.LEDMap[0] = rgb.LEDStore;
+		}
+		}else{
+			cout<<"d";
+			rgb.LEDStore = rgb.LEDMap[0];
+			for(int b = 0;b<COLORS::numberOfLeds;b++){
+				rgb.LEDMap[b] = rgb.LEDMap[b+1];
+		}
+		if(wrapAround){
+			rgb.LEDMap[COLORS::numberOfLeds - 1] = rgb.LEDStore;
+		}
+	}
+}
+cout<<endl;
+}
+
+
 
 int main(){
-	cout<<"a";
-	rgb.SetAll(COLORS::BLUE);
-	cout<<"b"<<endl;
+	/*
+	for(int i = 0; i<10;i++){
+		rgb.LEDMap[i].red = 255;
+		rgb.LEDMap[i].green = 15;
+		rgb.LEDMap[i].blue = 46;
+		
+		rgb.Set(i,rgb.LEDMap[i].color);
+	}
+*/
+	rgb.LEDMap[7].red = 255;
+	rgb.LEDMap[8].blue = 255;
+	rgb.LEDMap[9].green = 255;
+
+	rgb.RenderArray();
+	for(int g = 0;g<30;g++){
+	usleep(200000);
+	rgb.MoveArray(true,true,3);
+	rgb.RenderArray();
+	}
+
+
+
+	//rgb.Set(0,COLORS::RED);
+	//rgb.Set(1,COLORS::GREEN);
+	//rgb.Set(10,COLORS::BLUE);
+	//rgb.Render();
+	//usleep(5000000);
+	//rgb.SetAll(COLORS::ORANGE);
+	//rgb.Render();
+
 	return 0;
 }
